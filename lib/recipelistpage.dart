6f35194e2cb9f170a3e05d8ea521300d09f7b619
corea2244 <21600484@handong.edu>
2021-12-09 17:26:13 +0900

    Revised Appbar KangHyi 01

--- lib/recipelistpage.dart
+++ lib/recipelistpage.dart
@@ -49,20 +49,18 @@ class _RecipeListPageState extends State<RecipeListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: RecipeListPage.mainBackgroundColor,
       appBar: AppBar(
-        backgroundColor: Colors.white,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: RecipeListPage.mainColor, size: 30.0,),
+          icon: Icon(Icons.arrow_back, size: 30.0,),
           onPressed: (){
             Navigator.pop(context);
           },
         ),
         centerTitle: true,
-        title: Text('모앱개 레시피', style: TextStyle(color: Colors.black,),),
+        title: Text('모앱개 레시피',),
         actions: [
           IconButton(
-            icon: Icon(Icons.bookmark_border_outlined, color: Colors.black, size: 33.0,),
+            icon: Icon(Icons.bookmark_border_outlined, size: 33.0,),
             onPressed: (){
               Navigator.push(context, MaterialPageRoute(builder: (context) => BookmarkPage()),);
             },
